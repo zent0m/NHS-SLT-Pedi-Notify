8629d03356c0fb72931575b0da1b4b48749ef45a
zent0m <t.zulqarnain@hotmail.com>
2026-04-02 20:04:22 +0100

    add web server for render

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 import * as dotenv from "dotenv";
+import * as http from "http";
 import { JobScheduler } from "./scheduler";
 
 dotenv.config();
 
+const PORT = process.env.PORT || "3000";
 const mode = process.argv[2] || "once";
 
 const scheduler = new JobScheduler();
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("NHS SLT Pediatric Job Monitor is running");
+});
+
+server.listen(PORT, () => {
+  console.log(`Web server listening on port ${PORT}`);
+});
+
 process.on("SIGINT", () => {
   console.log("\nShutting down...");
   scheduler.stop();
+  server.close();
   process.exit(0);
 });
 
 process.on("SIGTERM", () => {
   console.log("\nShutting down...");
   scheduler.stop();
+  server.close();
   process.exit(0);
 });
 
@@ -24,6 +37,5 @@ if (mode === "schedule") {
 } else {
   scheduler.start().then(() => {
     console.log("Run complete. Use 'npm run start:schedule' for scheduled checks.");
-    process.exit(0);
   });
 }
